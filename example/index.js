5c24480e1a3f81e2ec6a0aada2192b149d807fb5
Roman Salnikov <roman@bardt.me>
2016-01-06 19:28:11 +0600

    Set up dev env

---
+++ example/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+class CropperDemo extends Component {
+  render() {
+    return <div>Cropper demo will go here</div>;
+  }
+}
+
+render(<CropperDemo/>, document.querySelector('app'));
